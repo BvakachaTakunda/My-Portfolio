584e1fa43eee177a180509b05202768327f1f907
BvakachaTakunda <takundabvax@gmail.com>
2024-09-03 03:05:37 -0700

    form

--- script.js
+++ script.js
@@ -1,26 +1,7 @@
-// function  toogle() {
-//     const menu = document.getElementById("menu");
-//     menu.classList.toggle("active");
-// }
-
-/* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
-// function myFunction() {
-//     var x = document.getElementById("myLinks");
-//     if (x.style.display === "block") {
-//       x.style.display = "none";
-//     } else {
-//       x.style.display = "block";
-//     }
-//   }
-
-
-
-
-
-
 // Nothing to see here guy's and gal's, only html + css.
 $(document).ready(function(){
 	$('#nav-icon1,#nav-icon2,#nav-icon3,#nav-icon4').click(function(){
 		$(this).toggleClass('open');
 	});
 });
+
